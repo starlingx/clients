cc5ca27c7f4ff452dfe45d39765a543f494b4544
Stefan Dinescu <stefan.dinescu@windriver.com>
2020-04-24 05:54:18 +0000

    Update stx-platformclients tag to stx.4.0-v1.1.0
    Change-Id: Ice55a06f8e0d32d1ac2373e29be8fde7391345f5
    Story: 2007518
    Task: 39513
    Signed-off-by: Stefan Dinescu <stefan.dinescu@windriver.com>

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.4.0-v1.0.0"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.4.0-v1.1.0"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:master-centos-stable-latest"
