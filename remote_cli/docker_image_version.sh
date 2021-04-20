19e87afefa96b475ad1dc65d99f16e969a97cd9b
Rafael Jordão Jardim <RafaelJordao.Jardim@windriver.com>
2021-04-20 12:30:53 -0400

    Update stx-platformclients tag to stx.5.0-v1.4.2
    This commit updates the image with the updated clients
    cgts-client and distributed-cloud-client to execute both
    in python2 and python3.
    
    Story: 2007106
    Task: 42315
    Depends-On: https://review.opendev.org/c/starlingx/root/+/787269
    Signed-off-by: Rafael Jordão Jardim <RafaelJordao.Jardim@windriver.com>
    Change-Id: Ifae4abe3a062c793cb2f7ced35994974519b17ab

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.5.0-v1.4.1"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.5.0-v1.4.2"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:master-centos-stable-latest"
