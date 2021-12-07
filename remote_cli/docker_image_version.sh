34e46bcdd9182a4e8f313d578f8321aaa14b73b1
Thiago Miranda <ThiagoOliveira.Miranda@windriver.com>
2021-12-07 08:47:01 -0500

    Update stx-platformclients image to version 1.5.3
    Updated image with the new fixes since the last build
    
    Partial-Bug: #1952400
    Depends-On: https://review.opendev.org/c/starlingx/root/+/820863
    
    Signed-off-by: Thiago Miranda <ThiagoOliveira.Miranda@windriver.com>
    Change-Id: I7283123fc3f6b1c3c5d0e4b590fc96bb667492c9

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.6.0-v1.5.2"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.6.0-v1.5.3"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:master-centos-stable-latest"
