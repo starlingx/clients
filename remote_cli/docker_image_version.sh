a641a464487fe910a9ab501e690e5d135988208f
Christopher Souza <Christopher.DeOliveiraSouza@windriver.com>
2023-06-28 08:56:09 -0300

    Update stx-platformclients image to stx.9.0-v1.5.11
    Updated image with distributedcloud-client changes
    
    Story: 2010584
    Task: 48300
    Depends-On: https://review.opendev.org/c/starlingx/root/+/887160
    
    Change-Id: I429e7fcff9b75ad047ea21547d41ea677d0d1b87
    Signed-off-by: Christopher Souza <Christopher.DeOliveiraSouza@windriver.com>

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.9.0-v1.5.10"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.9.0-v1.5.11"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:master-centos-stable-latest"
