3c38ad34d856e5ed013f1d0876f00cf126609988
Gustavo Herzmann <gustavo.herzmann@windriver.com>
2024-03-15 17:57:25 -0300

    Update stx-platformclients image to stx.9.0-v1.5.14
    Updated image with distributedcloud-client changes
    
    Story: 2010852
    Task: 49730
    Depends-On: https://review.opendev.org/c/starlingx/root/+/913445
    
    Change-Id: Ia057d013ac8eb6fb815d65edae1811cb83a246c3
    Signed-off-by: Gustavo Herzmann <gustavo.herzmann@windriver.com>

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.9.0-v1.5.13"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.9.0-v1.5.14"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:master-debian-stable-latest"
