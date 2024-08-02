4c793fb2969a2af952df935c7bb8c4f6c851ec66
Luiz Felipe Kina <LuizFelipe.EiskeKina@windriver.com>
2024-08-02 09:36:27 -0300

    Update stx-platformclients image to version stx.10.0-v1.5.15
    This commit updates the image to include software-client
    changes.
    
    Partial-bug: 2075756
    Depends-on: https://review.opendev.org/c/starlingx/root/+/925577
    
    Change-Id: Ib9dbb9a0e98ed4551b16d4cc7cddb41ac7203b49
    Signed-off-by: Luiz Felipe Kina <LuizFelipe.EiskeKina@windriver.com>

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.9.0-v1.5.14"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.10.0-v1.5.15"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:stx.10.0-v1.0.0"
