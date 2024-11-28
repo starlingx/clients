0d59e16b05638a5652ad5c71e3790fb5a10df807
Gustavo Ornaghi Antunes <gustavo.ornaghiantunes@windriver.com>
2024-11-28 08:53:57 -0300

    Update stx-platformclients image to version stx.10.0-v1.5.18
    Updated image with the new fixes since the last build
    
    Test Plan:
     - PASS: Execute kubectl command with remote cli
     - PASS: Execute system command with remote cli
    
    Story: 2011266
    Task: 51422
    Depends-On: https://review.opendev.org/c/starlingx/root/+/936443
    
    Change-Id: I20b80e03060930ae8dd55259112afc7fa19606b8
    Signed-off-by: Gustavo Ornaghi Antunes <gustavo.ornaghiantunes@windriver.com>

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.10.0-v1.5.17"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.10.0-v1.5.18"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:stx.10.0-v1.0.0"
