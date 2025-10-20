96e3634bf55301109fee64dacbdcbca6c4fef387
idoregol <Italo.doRegoLemos@windriver.com>
2025-10-20 15:39:18 -0300

    Update stx-platformclients image to version stx.11.0-v1.0.2
    Updated image with the new fixes since the last build
    
    Test Plan:
     - PASS: Execute kubectl command with remote cli
     - PASS: Execute oidc-auth command with remote cli
    
    Story: 2011563
    Task: 52940
    Depends-On: https://review.opendev.org/c/starlingx/root/+/964413
    
    Change-Id: Id15059b52857571f6267e384d7e830e57d5d3e0b
    Signed-off-by: idoregol <Italo.doRegoLemos@windriver.com>

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.11.0-v1.0.1"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.11.0-v1.0.2"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:stx.10.0-v1.0.0"
