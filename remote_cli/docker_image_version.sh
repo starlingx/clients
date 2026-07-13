bd40bb8c95322cfb46a8aa4938a69f27a71fc43b
lgosling <luizandre.gosling@windriver.com>
2026-07-13 18:15:41 -0300

    Update stx-platformclients tag to stx.13.0-v1.0.0-trixie
    Updated image with the new fixes since the last build
    
    Test Plan:
     - PASS: Execute system command with remote cli
     - PASS: Execute fm command with remote cli
    
    Story: 2011849
    Task: 55196
    
    Depends-On: https://review.opendev.org/c/starlingx/root/+/997227
    
    Change-Id: I68da084366e5a032b13a36b2ae48d272d5934f9a
    Signed-off-by: lgosling <luizandre.gosling@windriver.com>

--- remote_cli/docker_image_version.sh
+++ remote_cli/docker_image_version.sh
@@ -1,2 +1,2 @@
-export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.12.0-v1.0.5"
+export PLATFORM_DOCKER_IMAGE="docker.io/starlingx/stx-platformclients:stx.13.0-v1.0.0-trixie"
 export APPLICATION_DOCKER_IMAGE="docker.io/starlingx/stx-openstackclients:stx.10.0-v1.0.0"
